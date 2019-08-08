f625917d6ff9c14160a35f856afcf49be4b67be9
Eli Uriegas <eli.uriegas@docker.com>
2019-08-08 02:05:06 +0000

    wip new makefile
    Signed-off-by: Eli Uriegas <eli.uriegas@docker.com>

--- dockerversion/version_lib.go
+++ dockerversion/version_lib.go
@@ -1,17 +1,13 @@
-// +build !autogen
-
-// Package dockerversion is auto-generated at build-time
 package dockerversion // import "github.com/docker/docker/dockerversion"
 
 // Default build-time variable for library-import.
-// This file is overridden on build with build-time information.
-const (
-	GitCommit             = "library-import"
-	Version               = "library-import"
-	BuildTime             = "library-import"
-	IAmStatic             = "library-import"
-	InitCommitID          = "library-import"
-	PlatformName          = ""
-	ProductName           = ""
-	DefaultProductLicense = ""
+var (
+	GitCommit             string = "undefined"
+	Version               string = "undefined"
+	BuildTime             string = "undefined"
+	IAmStatic             string = "undefined"
+	InitCommitID          string = "undefined"
+	PlatformName          string = ""
+	ProductName           string = ""
+	DefaultProductLicense string = ""
 )
